26a432264bef2574cadf676ab3dca81c9d6c284b
Sergo <rogaldh@radsh.red>
2016-12-28 12:30:45 +0300

    Removed oldies

--- get_discovery/getDiscovery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DISCOVERY="$(curl -w '\n' 'https://discovery.etcd.io/new?size=3')";
-
-echo "Generated new discovery url: ${DISCOVERY}";
-
-awk -v token=$DISCOVERY '{gsub("{DISCOVERY_TOKEN}", token); print}' > ./cloud-config.yml < ./_cloud-config.yml;
